846eb6fca71efaa20a0f72f7ece42807ad9b5daa
Alexey <70944468+Alexey-Samodurov@users.noreply.github.com>
2024-11-20 21:50:19 +0300

    Add KubernetesPodOperator DAG examples for Spark and Ubuntu pods
    Introduce two new DAG examples: one for running a PySpark application on a Kubernetes cluster, and another for executing a basic Ubuntu pod with a simple bash command. Both DAGs leverage the KubernetesPodOperator from the Airflow Kubernetes provider.

---
+++ dags/example_k8s.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+with DAG(
+        dag_id='example_kubernetes_pod_operator',
+        schedule_interval=None,
+        start_date=None,
+    ) as dag:
+
+    k = KubernetesPodOperator(
+        namespace='default',
+        image="ubuntu:18.04",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "hello world", "&&", "sleep", "100"],
+        labels={"foo": "bar"},
+        name="example-pod",
+        task_id="run-example-pod",
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
---
+++ dags/example_spark_k8s_cluster.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+with DAG(
+        dag_id='pyspark_k8s_dag',
+        schedule_interval=None,
+        start_date=None,
+    ) as dag:
+
+    k = KubernetesPodOperator(
+        namespace='default',
+        image="samodurov/pyspark:latest",
+        cmds=["/opt/spark/bin/spark-submit"],
+        arguments=[
+            "--master", "k8s://https://kubernetes.default.svc:443",
+            "--name", "pyspark-app",
+            "--deploy-mode", "cluster",
+            "--conf", "spark.executor.memory=250m",
+            "--conf", "spark.driver.memory=500m",
+            "--conf", "spark.executor.instances=2",
+            "--conf", "spark.kubernetes.executor.request.cores=0.1",
+            "--conf", "spark.kubernetes.executor.limit.cores=0.1",
+            "--conf", "spark.kubernetes.driver.request.cores=0.1",
+            "--conf", "spark.kubernetes.driver.limit.cores=0.1",
+            # "--conf", "spark.executor.cores=1",
+            # "--conf", "spark.driver.cores=1",
+            "--conf", "spark.kubernetes.driver.container.image=samodurov/pyspark:latest",
+            "--conf", "spark.kubernetes.executor.container.image=samodurov/pyspark:latest",
+            "--conf", "spark.kubernetes.container.image.pullPolicy=Always",
+            "--conf", "spark.kubernetes.namespace=default",
+            "local:///opt/spark/examples/src/main/python/pi.py"
+        ],
+        labels={"foo": "bar"},
+        name="pyspark-app",
+        task_id="run_pyspark_app",
+        image_pull_policy="Always",
+        get_logs=True,
+        is_delete_operator_pod=False,
+    )
